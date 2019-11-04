fcf5080e6bd2316c4fed24dff83657ff6de3977c
jameijenda <jameijenda@gmail.com>
2019-11-04 01:58:42 -0300

    Added styling. App is on its final version

--- src/App.js
+++ src/App.js
@@ -14,12 +14,19 @@ class App extends Component {
       monsters: [],
       searchField: ''
     }
+
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }))
+  }
+
+  handleChange = e => {
+    this.setState({
+      searchField: e.target.value
+    })
   }
 
   render() {
@@ -29,11 +36,10 @@ class App extends Component {
 
     return (
       <div className='App'>
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
-          handleChange={e => this.setState({
-            searchField: e.target.value
-          })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>

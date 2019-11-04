d8587d90b27de5e36e2795654e119bdd2575744a
jameijenda <jameijenda@gmail.com>
2019-11-04 02:11:40 -0300

    Updates

---
+++ precache-manifest.b9b5923806cabc8d54043482ebad52bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7080eca375f466d5fe5b7f1f6decc9b6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "607c5fa24b17b4c2c83d",
+    "url": "/monsters-rolodex/static/css/main.f5734bcd.chunk.css"
+  },
+  {
+    "revision": "fbcd6677b09683eeab7a",
+    "url": "/monsters-rolodex/static/js/2.6a8dce3c.chunk.js"
+  },
+  {
+    "revision": "607c5fa24b17b4c2c83d",
+    "url": "/monsters-rolodex/static/js/main.036a3ba7.chunk.js"
+  },
+  {
+    "revision": "62a90dcdf724b9ab481e",
+    "url": "/monsters-rolodex/static/js/runtime-main.034b5f56.js"
+  }
+]);
